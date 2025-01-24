d23c5f51a9418515a216abad6c0d46f9feac6e63
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-25 00:12:57 +0545

    rotation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 function Box() {
+  // Reference to the mesh object
+  const meshRef = useRef();
+
+  // Use the useFrame hook to update the rotation on every frame
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01; // Rotate on the x-axis
+      meshRef.current.rotation.y += 0.01; // Rotate on the y-axis
+    }
+  });
+
   return (
-    <mesh rotation={[0.4, 0.2, 0]}>
+    <mesh ref={meshRef} rotation={[0.9, 0.34, 0.79]}>
       {/* A cube geometry */}
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[1.2, 1.3, 1.7]} />
       {/* A basic material */}
-      <meshStandardMaterial color="green" />
+      <meshStandardMaterial color="black" />
     </mesh>
   );
 }
 
 function App() {
   return (
-    <Canvas style={{ height: "100vh", background: "#272727" }}>
+    <Canvas style={{ height: "100vh", background: "white" }}>
       {/* Add a light */}
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.9} />
       {/* Add a rotating box */}
       <Box />
     </Canvas>
